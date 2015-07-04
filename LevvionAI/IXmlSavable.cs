ab1ff3426dc6ee7128d6ac808b52829b4e9647fd
Justin Hughart <justinhughart@gmail.com>
2015-07-04 01:32:20 -0500

    Imported IXmlLoadable and IXmlSavable from ESS.

---
+++ LevvionAI/IXmlSavable.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+namespace EssellSeriesStatistics
+{
+    /// <summary>
+    /// An interface that says you may save the object to XML.
+    /// </summary>
+    public interface IXmlSavable
+    {
+        /// <summary>
+        /// Converts the object to XML.
+        /// </summary>
+        /// <returns></returns>
+        XElement SaveToXml();
+    }
+}
