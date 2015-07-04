01e5818c84d7212e4331deb7076337695298a205
Justin Hughart <justinhughart@gmail.com>
2015-07-04 04:29:35 -0500

    Completed BoolCondition.

--- LevvionAI/Types/Conditions/BoolCondition.cs
+++ LevvionAI/Types/Conditions/BoolCondition.cs
@@ -1,7 +1,74 @@
-﻿namespace LevvionAI.Types.Conditions
+﻿using System;
+using System.Xml.Linq;
+
+namespace LevvionAI.Types.Conditions
 {
+    /// <summary>
+    /// A condition for checking a bool.
+    /// </summary>
     public class BoolCondition : Condition
     {
+        /// <summary>
+        /// The value
+        /// </summary>
         public bool Value;
+
+        /// <summary>
+        /// Determines whether this instance is satisfied.
+        /// </summary>
+        /// <param name="state">The state of the AI.</param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception">Invalid equality type in BoolCondtion.IsSatisfied().</exception>
+        public override bool IsSatisfied(AIState state)
+        {
+            var value = state.GetBool(ID);
+
+            if (!value.HasValue)
+            {
+                return false;
+            }
+
+            switch (Equality)
+            {
+                    case EqualityType.Equals:
+                    return value == Value;
+                    case EqualityType.NotEquals:
+                    return value != Value;
+                default:
+                    throw new Exception("Invalid equality type in BoolCondtion.IsSatisfied().");
+            }
+        }
+
+        /// <summary>
+        /// Uses XML to initialize the object.
+        /// </summary>
+        /// <param name="element">The element used for loading.</param>
+        public override void LoadFromXml(XElement element)
+        {
+            base.LoadFromXml(element);
+
+            //Handle attributes
+            foreach (var attribute in element.Attributes())
+            {
+                if (attribute.ToStringEquals("value"))
+                {
+                    bool.TryParse(attribute.Value, out Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts the object to XML.
+        /// </summary>
+        /// <returns></returns>
+        public override XElement SaveToXml()
+        {
+            var element = base.SaveToXml();
+            element.Name = "boolcondition";
+
+            element.Add(new XAttribute("value", Value));
+
+            return element;
+        }
     }
 }
