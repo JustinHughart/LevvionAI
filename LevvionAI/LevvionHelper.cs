caea10795ed555887166389492fb98997f6bd3c8
Justin Hughart <justinhughart@gmail.com>
2015-07-04 03:04:02 -0500

    Added LevvionHelper.

---
+++ LevvionAI/LevvionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LevvionAI
+{
+    public static class LevvionHelper
+    {
+        #region String Equality
+
+        /// <summary>
+        /// Converts the object to a string and checks if it's equal ordinally, ignoring case.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns></returns>
+        public static bool ToStringEquals(this object a, string b)
+        {
+            return a.ToString().Equals(b, StringComparison.OrdinalIgnoreCase);
+        }
+
+        #endregion
+    }
+}
