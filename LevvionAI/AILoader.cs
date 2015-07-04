2e703398b4f27f4c54282a2fd1f396a7a10c9af0
Justin Hughart <justinhughart@gmail.com>
2015-07-04 04:06:26 -0500

    Created the AI loader.

---
+++ LevvionAI/AILoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Linq;
+using LevvionAI.Types;
+using LevvionAI.Types.Conditions;
+
+namespace LevvionAI
+{
+    /// <summary>
+    /// A static class for loading various bits of data when it comes to AI.
+    /// </summary>
+    public static class AILoader
+    {
+        /// <summary>
+        /// Creates the condition.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <returns></returns>
+        public static Condition CreateCondition(XElement element)
+        {
+            Condition condition = null;
+
+            //Certainly there's a more elegant way than this? Switch cases don't work on strings apparantly.
+            if (element.Name.LocalName.ToStringEquals("multicondition"))
+            {
+                condition = new MultiCondition();
+            }
+            else if (element.Name.LocalName.ToStringEquals("boolcondition"))
+            {
+                condition = new BoolCondition();
+            }
+            else if (element.Name.LocalName.ToStringEquals("intcondition"))
+            {
+                condition = new IntCondition();
+            }
+            else if (element.Name.LocalName.ToStringEquals("floatcondition"))
+            {
+                condition = new FloatCondition();
+            }
+            else
+            {
+                return null;
+            }
+
+            condition.LoadFromXml(element);
+
+            return condition;
+        }
+    }
+}
