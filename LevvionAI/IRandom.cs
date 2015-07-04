ca7c286d2aef08cef152ce866cd9e1604f5edec6
Justin Hughart <justinhughart@gmail.com>
2015-07-04 01:58:35 -0500

    Created IRandom.

---
+++ LevvionAI/IRandom.cs
@@ -0,0 +1,28 @@
+﻿namespace LevvionAI
+{
+    /// <summary>
+    /// An interface to get data from a RNG.
+    /// </summary>
+    public interface IRandom
+    {
+        /// <summary>
+        /// Gets a random bool.
+        /// </summary>
+        /// <returns></returns>
+        bool GetBool();
+        /// <summary>
+        /// Gets a random int between min and max.
+        /// </summary>
+        /// <param name="min">The minimum.</param>
+        /// <param name="max">The maximum.</param>
+        /// <returns></returns>
+        int GetInt(int min, int max);
+        /// <summary>
+        /// Gets a random float between min and max.
+        /// </summary>
+        /// <param name="min">The minimum.</param>
+        /// <param name="max">The maximum.</param>
+        /// <returns></returns>
+        float GetFloat(float min, float max);
+    }
+}
