40120ee648f3d5b3a2b22b33599d38348cb80f73
Justin Hughart <justinhughart@gmail.com>
2015-07-12 03:58:45 -0500

    Added TimeControl.

---
+++ LevvionAITool/TimeControl.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace LevvionAITool
+{
+    partial class TimeControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numDays = new System.Windows.Forms.NumericUpDown();
+            this.numHours = new System.Windows.Forms.NumericUpDown();
+            this.numMinutes = new System.Windows.Forms.NumericUpDown();
+            this.numSeconds = new System.Windows.Forms.NumericUpDown();
+            this.numMilliseconds = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numDays)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSeconds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMilliseconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Days: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hours: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Minutes: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Seconds: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Milliseconds: ";
+            // 
+            // numDays
+            // 
+            this.numDays.Location = new System.Drawing.Point(84, 4);
+            this.numDays.Name = "numDays";
+            this.numDays.Size = new System.Drawing.Size(120, 20);
+            this.numDays.TabIndex = 5;
+            // 
+            // numHours
+            // 
+            this.numHours.Location = new System.Drawing.Point(84, 32);
+            this.numHours.Name = "numHours";
+            this.numHours.Size = new System.Drawing.Size(120, 20);
+            this.numHours.TabIndex = 6;
+            // 
+            // numMinutes
+            // 
+            this.numMinutes.Location = new System.Drawing.Point(84, 62);
+            this.numMinutes.Name = "numMinutes";
+            this.numMinutes.Size = new System.Drawing.Size(120, 20);
+            this.numMinutes.TabIndex = 7;
+            // 
+            // numSeconds
+            // 
+            this.numSeconds.Location = new System.Drawing.Point(84, 88);
+            this.numSeconds.Name = "numSeconds";
+            this.numSeconds.Size = new System.Drawing.Size(120, 20);
+            this.numSeconds.TabIndex = 8;
+            // 
+            // numMilliseconds
+            // 
+            this.numMilliseconds.Location = new System.Drawing.Point(84, 114);
+            this.numMilliseconds.Name = "numMilliseconds";
+            this.numMilliseconds.Size = new System.Drawing.Size(120, 20);
+            this.numMilliseconds.TabIndex = 9;
+            // 
+            // TimeControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.numMilliseconds);
+            this.Controls.Add(this.numSeconds);
+            this.Controls.Add(this.numMinutes);
+            this.Controls.Add(this.numHours);
+            this.Controls.Add(this.numDays);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TimeControl";
+            this.Size = new System.Drawing.Size(212, 141);
+            ((System.ComponentModel.ISupportInitialize)(this.numDays)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSeconds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMilliseconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numDays;
+        private System.Windows.Forms.NumericUpDown numHours;
+        private System.Windows.Forms.NumericUpDown numMinutes;
+        private System.Windows.Forms.NumericUpDown numSeconds;
+        private System.Windows.Forms.NumericUpDown numMilliseconds;
+    }
+}
---
+++ LevvionAITool/TimeControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LevvionAITool
+{
+    /// <summary>
+    /// A control for setting time.
+    /// </summary>
+    public partial class TimeControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeControl"/> class.
+        /// </summary>
+        public TimeControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the time for this instance.
+        /// </summary>
+        /// <returns></returns>
+        public TimeSpan Time()
+        {
+            return new TimeSpan((int)numDays.Value, (int)numHours.Value, (int)numMinutes.Value, (int)numSeconds.Value, (int)numMilliseconds.Value);
+        }
+    }
+}
