2ff2e2913877b370fc723d878f48b454535b1259
Justin Hughart <justinhughart@gmail.com>
2015-07-12 02:18:56 -0500

    Updated MainForm.

--- LevvionAITool/MainForm.Designer.cs
+++ LevvionAITool/MainForm.Designer.cs
@@ -28,12 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lstCircles = new System.Windows.Forms.ListBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(719, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItemClick);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItemClick);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItemClick);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStripMenuItemClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
+            // 
+            // lstCircles
+            // 
+            this.lstCircles.FormattingEnabled = true;
+            this.lstCircles.Location = new System.Drawing.Point(12, 27);
+            this.lstCircles.Name = "lstCircles";
+            this.lstCircles.Size = new System.Drawing.Size(694, 459);
+            this.lstCircles.TabIndex = 1;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(12, 493);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(93, 493);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 3;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(174, 493);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(719, 528);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.lstCircles);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Mask Of Levvion";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListBox lstCircles;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
 
--- LevvionAITool/MainForm.cs
+++ LevvionAITool/MainForm.cs
@@ -1,19 +1,273 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace LevvionAITool
 {
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// The path to the file we're working on.
+        /// </summary>
+        private string _filepath;
+
         public MainForm()
         {
             InitializeComponent();
         }
+
+        private void CreateNewProfile()
+        {
+            _filepath = "";
+            ChangeTitle();
+
+            lstCircles.
+        }
+
+        /// <summary>
+        /// Changes the title of the window.
+        /// </summary>
+        private void ChangeTitle()
+        {
+            Text = "Essell Series Stat Profile Editor - " + _filepath;
+
+            if (_filepath.Equals(""))
+            {
+                Text += "New Profile";
+            }
+        }
+
+        /// <summary>
+        /// Triggers when the new button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void NewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            CreateNewProfile();
+        }
+
+        /// <summary>
+        /// Triggers when the open button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OpenToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Essell Series Stat Profile (*.esp)|*.esp";
+            ofd.ShowDialog();
+
+            if (ofd.FileName == "")
+            {
+                return;
+            }
+
+            CreateNewProfile(); //Initialize the data.
+
+            _filepath = ofd.FileName;
+
+            ChangeTitle();
+
+            LoadFromFile(_filepath);
+        }
+
+        /// <summary>
+        /// Triggers when the save button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void SaveToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            if (_filepath == "")
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Essell Series Stat Profile (*.esp)|*.esp";
+                sfd.ShowDialog();
+
+                if (sfd.FileName != "")
+                {
+                    _filepath = sfd.FileName;
+
+                    ChangeTitle();
+                }
+            }
+
+            if (_filepath != "")
+            {
+                SaveToFile(_filepath);
+            }
+        }
+
+        /// <summary>
+        /// Triggers when the save as button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void SaveAsToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            bool valid = false;
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Essell Series Stat Profile (*.esp)|*.esp";
+            sfd.ShowDialog();
+
+            if (sfd.FileName != "")
+            {
+                valid = true;
+                _filepath = sfd.FileName;
+                ChangeTitle();
+            }
+
+            if (valid)
+            {
+                SaveToFile(_filepath);
+            }
+        }
+
+        /// <summary>
+        /// Triggers when the exit button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void ExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Saves to file.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <exception cref="System.ArgumentException">File type not supported.</exception>
+        private void SaveToFile(string path)
+        {
+            path = ValidatePath(path);
+
+            //Modifiers
+            var modifiers = new Dictionary<StatType, float>();
+            modifiers.Add(StatType.Vitality, (float)numVIT.Value);
+            modifiers.Add(StatType.Wisdom, (float)numWIS.Value);
+            modifiers.Add(StatType.Stamina, (float)numSTA.Value);
+            modifiers.Add(StatType.Strength, (float)numSTR.Value);
+            modifiers.Add(StatType.Defense, (float)numDEF.Value);
+            modifiers.Add(StatType.Intelligence, (float)numINT.Value);
+            modifiers.Add(StatType.Resilience, (float)numRES.Value);
+            modifiers.Add(StatType.Speed, (float)numSPD.Value);
+            modifiers.Add(StatType.Accuracy, (float)numACC.Value);
+            modifiers.Add(StatType.Evade, (float)numEVD.Value);
+            modifiers.Add(StatType.Luck, (float)numLUK.Value);
+
+            //Stats
+            var stats = new Dictionary<StatType, ProfileStrength>();
+
+            foreach (DataGridViewRow row in dgvStats.Rows)
+            {
+                StatType type = StatType.None;
+                ProfileStrength strength = ProfileStrength.ResistElementHigh;
+
+                if (row.Cells[0].Value == null || row.Cells[1].Value == null)
+                {
+                    continue;
+                }
+
+                type = (StatType)row.Cells[0].Value;
+                strength = (ProfileStrength)row.Cells[1].Value;
+
+                stats.Add(type, strength);
+            }
+
+            //Basic Data
+            var profile = new StatProfile(modifiers, stats);
+            profile.Name = txtName.Text;
+            profile.Description = txtDescription.Text;
+
+            //First we'll back up the old file.
+            if (File.Exists(path))
+            {
+                string backuppath = path + ".bak";
+
+                if (File.Exists(backuppath))
+                {
+                    File.Delete(backuppath);
+                }
+
+                File.Copy(path, backuppath);
+                File.Delete(path);
+            }
+
+            //Then save it!
+            var doc = new XDocument(profile.SaveToXml());
+
+            using (var stream = File.OpenWrite(path))
+            {
+                doc.Save(stream);
+            }
+        }
+
+        /// <summary>
+        /// Loads from file.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <exception cref="System.ArgumentException">File type not supported.</exception>
+        /// <exception cref="System.Xml.XmlException">
+        /// No root.
+        /// or
+        /// Root is not an entity.
+        /// </exception>
+        private void LoadFromFile(string path)
+        {
+            path = ValidatePath(path);
+
+            //Load the profile.
+            XDocument doc = XDocument.Load(path);
+            var profile = new StatProfile();
+            profile.LoadFromXml(doc.Root);
+
+            //Assign basic data.
+            txtName.Text = profile.Name;
+            txtDescription.Text = profile.Description;
+
+            //Assign modifiers.
+            numVIT.Value = (decimal)profile.GetModifier(StatType.Vitality);
+            numWIS.Value = (decimal)profile.GetModifier(StatType.Wisdom);
+            numSTA.Value = (decimal)profile.GetModifier(StatType.Stamina);
+            numSTR.Value = (decimal)profile.GetModifier(StatType.Strength);
+            numDEF.Value = (decimal)profile.GetModifier(StatType.Defense);
+            numINT.Value = (decimal)profile.GetModifier(StatType.Intelligence);
+            numRES.Value = (decimal)profile.GetModifier(StatType.Resilience);
+            numSPD.Value = (decimal)profile.GetModifier(StatType.Speed);
+            numACC.Value = (decimal)profile.GetModifier(StatType.Accuracy);
+            numEVD.Value = (decimal)profile.GetModifier(StatType.Evade);
+            numLUK.Value = (decimal)profile.GetModifier(StatType.Luck);
+
+            //Assign stats.
+            var stats = profile.GetPrivateDataCopy().Item2;
+
+            foreach (var stat in stats)
+            {
+                var row = new object[2];
+                row[0] = stat.Key;
+                row[1] = stat.Value;
+                dgvStats.Rows.Add(row);
+            }
+        }
+
+        /// <summary>
+        /// Validates the path. I had wierd issues with paths, which is why this exists.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns></returns>
+        private string ValidatePath(string input)
+        {
+            while (input.EndsWith(" "))
+            {
+                input.Remove(input.Length - 2);
+            }
+
+            return input;
+        }
+
+
+
     }
 }
